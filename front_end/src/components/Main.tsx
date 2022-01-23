a88e048d83d8ac3d75ad330e193cfed3aaf1a0cd
Erick C <cherick04@hotmail.com>
2022-01-23 17:55:51 -0300

    Updating front end with brownie config file

---
+++ front_end/src/components/Main.tsx
@@ -0,0 +1,9 @@
+
+export const Main = () => {
+    // Show token values from wallet
+    // Get the address from different tokens
+    // Get the balance of the users wallet
+
+    // Send the brownie-config to the `src` folder
+    // Send the build folder
+}
